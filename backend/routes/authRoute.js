3951a4e06663d1c7a27fc34583ee78a129f3b90a
nhân <nhanle.lx@gmail.com>
2022-10-18 18:01:53 +0700

    authRoute

--- backend/routes/authRoute.js
+++ backend/routes/authRoute.js
@@ -1,9 +1,10 @@
 const express = require("express");
+const authController = require("../controllers/authController");
+const middlewareController = require("../controllers/middlewareController");
 const router = express.Router();
 
-/* GET home page. */
-router.get("/", function (req, res, next) {
-  res.json({ title: "oke" });
-});
-
+router.post("/register", authController.register);
+router.post("/login", authController.login);
+router.post("/refreshToken", authController.refreshToken);
+router.post("/logout", middlewareController.verifyToken, authController.logout);
 module.exports = router;
